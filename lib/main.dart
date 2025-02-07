04d416e4381fbaaec7e45af3e1c293546e9850d9
Steven DR <145115889+sdr20@users.noreply.github.com>
2025-02-07 23:45:51 +0800

    pipes

--- lib/main.dart
+++ lib/main.dart
@@ -62,14 +62,13 @@ class _TemperatureScreenState extends State<TemperatureScreen> {
     double heightFactor = (temperature - 20) / 10;
     heightFactor = heightFactor < 0 ? 0 : heightFactor > 1 ? 1 : heightFactor;
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
       children: [
         Text(
           label,
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
         ),
-        SizedBox(width: 20),
+        SizedBox(height: 10),
         Container(
           width: 100,
           height: 200,
@@ -115,13 +114,26 @@ class _TemperatureScreenState extends State<TemperatureScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              _buildThermometer(temperatures[3], Colors.purple, 'Sensor 4'),
-              SizedBox(height: 20),
-              _buildThermometer(temperatures[2], Colors.green, 'Sensor 3'),
-              SizedBox(height: 20),
-              _buildThermometer(temperatures[1], Colors.blue, 'Sensor 2'),
-              SizedBox(height: 20),
-              _buildThermometer(temperatures[0], Colors.red, 'Sensor 1'),
+              Container(
+                width: 200,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.grey[600],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    _buildThermometer(temperatures[3], Colors.purple, 'Sensor 4'),
+                    SizedBox(height: 20),
+                    _buildThermometer(temperatures[2], Colors.green, 'Sensor 3'),
+                    SizedBox(height: 20),
+                    _buildThermometer(temperatures[1], Colors.blue, 'Sensor 2'),
+                    SizedBox(height: 20),
+                    _buildThermometer(temperatures[0], Colors.red, 'Sensor 1'),
+                  ],
+                ),
+              ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
